0b941ee21db886f2d2e6d7bdda8cc15a03cff274
Abhishek <sil.abhishek97@gmail.com>
2018-04-20 21:18:30 +0530

    II

--- effect.js
+++ effect.js
@@ -23,7 +23,7 @@ $('document').ready(function(){
 		$('#bulb_green').addClass('bulb-glow-green');
 		$('#bulb_pink').addClass('bulb-glow-pink');
 		$('#bulb_orange').addClass('bulb-glow-orange');
-		$('body').addClass('peach');
+		$('body').addClass('cb-slideshow');
 		$(this).fadeOut('slow').delay(5000).promise().done(function(){
 			$('#play').fadeIn('slow');
 		});
